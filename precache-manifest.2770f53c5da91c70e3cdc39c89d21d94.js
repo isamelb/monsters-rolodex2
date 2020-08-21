e2e7ca539e51ce195ae313a7e96a5d1935444b32
isam <isamelb@gmail.com>
2020-08-21 12:32:11 +0100

    Updates

---
+++ precache-manifest.2770f53c5da91c70e3cdc39c89d21d94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408e330b32959be283d1ef84c4a1d4bc",
+    "url": "/monsters-rolodex2/index.html"
+  },
+  {
+    "revision": "d70afefab217ecd528bc",
+    "url": "/monsters-rolodex2/static/css/main.d5c42035.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex2/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex2/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d70afefab217ecd528bc",
+    "url": "/monsters-rolodex2/static/js/main.622d8004.chunk.js"
+  },
+  {
+    "revision": "5129b2734e446f0ec2c6",
+    "url": "/monsters-rolodex2/static/js/runtime-main.e8342c1e.js"
+  }
+]);
